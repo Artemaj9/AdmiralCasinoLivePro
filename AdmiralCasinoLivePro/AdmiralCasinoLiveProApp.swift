be56275180a58de9f758771415ddbe272663f3d4
ArtemMoiseev <moiseev@physics.msu.ru>
2024-01-24 00:25:37 +0300

    Initial Commit

---
+++ AdmiralCasinoLivePro/AdmiralCasinoLiveProApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdmiralCasinoLiveProApp.swift
+//  AdmiralCasinoLivePro
+//
+//  Created by Artem on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AdmiralCasinoLiveProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
